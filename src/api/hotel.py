c1173896b57da01f5404b08416291d5e79f58d95
w0mb <danil-kovalev-04@mail.ru>
2025-02-01 21:44:02 +0700

    change get, delete and put endpoints

--- src/api/hotel.py
+++ src/api/hotel.py
@@ -4,7 +4,7 @@ from src.chemas.chema import hotel, hotelPatch, Config
 from src.api.status import OK_JSON, NOTFOUND_JSON
 from src.db import new_session
 
-from sqlalchemy import insert, select
+from sqlalchemy import insert, select, delete, update
 from src.models.hotel_models import HotelsOrm
 
 router = APIRouter(prefix="/hotels", tags=["Отели"])
@@ -19,20 +19,24 @@ async def get_hotels(pagination: PaginationDep,
 
 
     async with new_session() as session:
+        limit = pagination.count_ipp
+        offset = (pagination.page - 1) * pagination.count_ipp
+
+
         query = select(HotelsOrm)
+
+        if title:
+            query = query.where(HotelsOrm.title.ilike(f"%{title}%"))
+        if location:
+            query = query.where(HotelsOrm.location.ilike(f"%{location}%"))
+
+
+        query = query.limit(limit).offset(offset)
+
 
         result = await session.execute(query)
         hotels = result.scalars().all()
-        print("Hotels without filters:", hotels)
-        if title:
-            hotels = [h for h in hotels if title.lower() in h.title.lower()]
-        if location:
-            hotels = [h for h in hotels if location.lower() in h.location.lower()]
 
-
-        start = (pagination.page - 1) * pagination.count_ipp
-        end = start + pagination.count_ipp
-        hotels = hotels[start:end]
         return hotels
 
 
@@ -42,30 +46,30 @@ async def add_hotel(hotel_data: hotel = Body(openapi_examples=HOTEL_EXAMPLES)):
         add_hotel_stmt = insert(HotelsOrm).values(**hotel_data.model_dump())
         await session.execute(add_hotel_stmt)
         await session.commit()
-
-    return OK_JSON
+        return OK_JSON
 
 
 @router.delete("/{hotel_id}")
-def delete_hotel(hotel_id: int = Path(description="ID отеля для удаления", example=1)):
-    global hotels
-    hotel_to_remove = next((hotel for hotel in hotels if hotel["id"] == hotel_id), None)
-    if hotel_to_remove:
-        hotels.remove(hotel_to_remove)
+async def delete_hotel(hotel_id: int = Path(description="ID отеля для удаления", example=1)):
+    async with new_session() as session:
+        delete_hotel_stmt = delete(HotelsOrm).where(HotelsOrm.id == hotel_id)
+        await session.execute(delete_hotel_stmt)
+        await session.commit()
         return OK_JSON
-    else:
-        return NOTFOUND_JSON
 
-
+# Полное изменение чего-то
+# В контексте задачи меняем запись в БД по id отеля
 @router.put("/{hotel_id}")
-def change_hotel_put(hotel_id: int = Path(description="ID отеля для Полного изменения", example=1), hotel_data: hotel = Body(examples=HOTEL_EXAMPLES)):
-    global hotels
-    for hotel_item in hotels:
-        if hotel_item["id"] == hotel_id:
-            hotel_item["title"] = hotel_data.title
-            hotel_item["rate"] = hotel_data.rate
-            return OK_JSON
-    return NOTFOUND_JSON
+async def change_hotel_put(hotel_id: int = Path(description="ID отеля для Полного изменения", example=1), hotel_data: hotel = Body(examples=HOTEL_EXAMPLES)):
+    async with new_session() as session:
+        update_hotel_stmt = (update(HotelsOrm).
+                             where(HotelsOrm.id==hotel_id).
+                             values(title=hotel_data.title, location=hotel_data.location))
+
+
+        await session.execute(update_hotel_stmt)
+        await session.commit()
+        return OK_JSON
 
 
 @router.patch("/{hotel_id}")
