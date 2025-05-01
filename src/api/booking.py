a020fbf1420dd4680c03b75c12f81284d174d4d9
w0mb <w0mb@users.noreply.github.com>
2025-05-01 19:30:14 +0700

    add get endpoints for booking

--- src/api/booking.py
+++ src/api/booking.py
@@ -6,7 +6,12 @@ from src.api.dependencies import UserDependecies
 
 router = APIRouter(prefix="/bookings", tags=["Бронирование Номеров в отелях"])
 
-@router.post("/create/{booking_id}")
+@router.post("/create/{booking_id}",
+             name="Создать бронирование",
+             description="Создает новое бронирование с помощью DBmanager\n\n"
+             "Создавать бронь имеет право только аутентифицированный пользователь\n\n"
+             "Получаем id такого пользователя через get_current_user_id из dependencies"
+        )
 async def create_booking(
     db: DBDep,
     user_id: UserDependecies,
@@ -25,3 +30,18 @@ async def create_booking(
     await db.commit()
     
     return {"status": "ok", "data": res_data}
+
+@router.get("",
+            name="Получить все бронирования",
+            description="Возвращает все бронирования всех пользователей посредством DBmanager"
+        )
+async def get_bookings(db: DBDep):
+    return await db.bookings.get_all()
+
+@router.get("/me",
+            name="Получить бронирования у текущего пользователя",
+            description="Возвращает все бронирования текущего пользователя посредством DBmanager\n\n"
+            "и get_current_user_id из dependencies"
+        )
+async def get_my_bookings(db: DBDep, user_id: UserDependecies):
+    return await db.bookings.get_filtred(user_id=user_id) 
